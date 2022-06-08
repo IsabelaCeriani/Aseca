4c68643ac6265c10c19bd64cb0c25a2fd07a1eb3
IsabelaCeriani <isa>
2022-06-08 16:28:05 -0300

    Initial commit

--- src/main/java/com/example/aseca/AsecaApplication.java
+++ src/main/java/com/example/aseca/AsecaApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.aseca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class AsecaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(AsecaApplication.class, args);
     }
 
 }
---
+++ src/test/java/com/example/aseca/AsecaApplicationTests.java
@@ -0,0 +1,10 @@
+package com.example.aseca;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsecaApplicationTests {
+
+
+
+}
